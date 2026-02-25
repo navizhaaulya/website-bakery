162140b5c66391badb6001e6cfd90ea37999518a
navizhaaulya <navytzie@gmail.com>
2026-02-25 11:27:30 +0700

    fix app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ function App() {
   return (
     <>
       <h1>HALO</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus et dolor blanditiis iste! Nihil aliquam vitae fugiat placeat. Nemo enim quae modi dignissimos accusamus vero repudiandae, nobis dolorum fuga quibusdam.</p>
     </>
   )
 }
