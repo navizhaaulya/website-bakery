defe17ead34d0403e1890e17f4b8cff49b935e06
navizhaaulya <navytzie@gmail.com>
2026-02-25 11:23:21 +0700

    initial

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { Navbar } from './components/Navbar.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <div className='container mx-auto px-0'>
+    <Navbar />
+    <App />
+
+    </div>
+  </StrictMode>,
+);
