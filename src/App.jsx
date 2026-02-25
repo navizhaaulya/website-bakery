fcc4cd27acf819d9432ddb908ea2ef1dc66d9c8f
navizhaaulya <navytzie@gmail.com>
2026-02-25 11:37:39 +0700

    fix structure file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import { Navbar } from "./components/Navbar"
+import About from "./pages/About"
 
 function App() {
 
-
   return (
     <>
+    <Navbar />
       <h1>HALO</h1>
       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus et dolor blanditiis iste! Nihil aliquam vitae fugiat placeat. Nemo enim quae modi dignissimos accusamus vero repudiandae, nobis dolorum fuga quibusdam.</p>
+      <About />
+      
     </>
   )
 }
---
+++ src/pages/About.jsx
@@ -0,0 +1,9 @@
+const About = () => {
+    return (
+        <div>
+            <h1 className="text-3xl font-semibold">Ini halaman About</h1>
+        </div>
+    )
+}
+
+export default About
