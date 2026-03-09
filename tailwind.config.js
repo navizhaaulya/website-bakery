0cec24663b68c3e74b41b5532594cfd46e1d7550
navizhaaulya <navytzie@gmail.com>
2026-03-09 18:11:57 +0700

    update font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        heading: ["Montserrat", "sans-serif"],
+        body: ["Lato", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 }
